eae35da5360a8ed03023e07eb8be45a850e18ce2
Tim Menzies <tim.menzies@gmail.com>
2020-02-18 15:40:34 -0500

    saving

--- src/rule.lisp
+++ src/rule.lisp
@@ -1,17 +1,20 @@
-
-(defstruct ranges arr n epsilon 
-           (fx #'first)
-           (fy #'second)
-           (fxs #'make-num)
-           (fys #'make-num)
-           xs
-           ys)
+(defstruct ranges 
+  arr n epsilon
+  (skip "?")
+  (fx #'first)
+  (fy #'second)
+  (fxs #'make-num)
+  (fys #'make-num)
+  first last
+  (put #'(lambda (x rank) (slot-value x 'rank) rank))
+  xs
+  ys)
 
 (defmethod x ((r ranges) z) (funcall (? r fx)  z))
 (defmethod y ((r ranges) z) (funcall (? r fy)  z))
 
 (defmethod create ((r ranges) lst &key epsilon (cohen 0.3))
-  (with-slots (n arr fxs fys epsilon xs ys) r
+  (with-slots (n arr fxs fys epsilon xs ys first last) r
     (setf n   (length lst)
           arr (coerce
                (sort rows #'(lambda (a b) (< (x r a) (x r b))))
@@ -19,18 +22,21 @@
           xs  (funcall fxs)
           ys  (funcall fys))
     (dolist (item lst)
+      (setf first (or first (x r item))
+            last  (x r item))
       (update xs (x r item))
       (update ys (y r item)))
     (setf epsilon (or epsilon
                       (* cohen (spread xs))))))
 
-(defmethod div ((r ranges)  &key ys1 (lo 0) (hi (? r n)))
+(defmethod div ((r ranges)  &key right (lo 0) (hi (? r n)))
+   (with-slots 
   (let (cut
-        (xs1 (funcall (? r fxs)))
-        (safe (copy-structure ys1)))
-    (loop for i from lo to hi
-       for item = (aref (? r arr) i) do
-         (let ((x1 (x r item))
+        (min  (spread right))
+        (xs1  (funcall (? r fxs)))
+        (safe (copy-structure right)))
+    (loop for i from lo to hi do
+         (let* ((item (aref (? r arr) i)))
                (y1 (y r item)))
            (if (not (equalp x1 "?"))
                (update xs1 x1)
