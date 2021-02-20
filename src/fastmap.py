62a9e10db04170dd2904d14601675fab88f13f37
Tim Menzies <timm@ieee.org>
2021-02-20 13:53:20 -0500

    saving

--- src/fastmap.py
+++ src/fastmap.py
@@ -12,10 +12,14 @@ from it import it
 from csv import csv
 
 def anExample():
-  rows=[row for row in csv("../data/auto93.csv")]
-  assert 399== len(rows)
-  assert float is type(rows[1][4])
-  assert int   is type(rows[1][0])
+  t= Tab()
+  for row in csv("../data/auto93.csv"): 
+     print(row); t.add(row)
+  assert 398== len(t.rows)
+  print(t.rows[1])
+  #assert float is type(t.rows[1].cells[4])
+  #assert int   is type(t.rows[1].cells[0])
+  print(len(t.rows))
     
 def Row(t,lst):
   def dist(i,j):
@@ -61,85 +65,78 @@ def Num(pos=0, txt="", w=1):
   return it(pos=pos, txt=txt, w=w, n=0, _all=[]) + locals()
 
 def Cols(): 
-  def add(i,pos,txt)
+  def add(i,pos,txt):
     weight = -1 if "-" in txt else 1
     where  = ([]   if "?" in txt else (i.y if txt[-1] in "+-"  else i.x))
-    what   = (Skip in "?" in txt else (Num if txt[0].isupper() else Sym))
+    what   = (Skip if "?" in txt else (Num if txt[0].isupper() else Sym))
     now    = what(pos,txt,weight)
     where += [now]
     return now
   return it(all=[],x=[],y=[]) + locals()
 
-def Tab(src,using="y",p=2):
-  def _row(i,lst):  return Row(i, [col.add(x) for col,x in zip(i.cols.all,lst)])
-  def _cols(i,lst): return [i.cols.add(pos,xt) for pos,txt in enumerate(list)]
-  def adds(i,src): [add(i,row) for row in src]
-  def add(i,lst):
+def Tab(using="y",p=2, fast=False):
+  def _row(i,lst)  : return Row(i, [add(col,x) for col,x in zip(i.cols.all,lst)])
+  def _cols(i,lst) : return [i.cols.add(pos,txt) for pos,txt in enumerate(lst)]
+  def cluster(i)   : return cluster(i.rows, fast)
+  def adds(i,src)  : [add(i,row) for row in src]; return i
+  def add(i,lst)   :
     if i.cols.all: 
       i.rows += [_row(i,lst)]
     else:
       i.cols.all  = _cols(i,lst)
       i.header=lst
+  return it(header=[], rows=[], cols=Cols()) + locals()
 
-def poles(rows,fast):
-  if fast:
-    anyone  = random.choice(rows)
-    north,_ = anyone.furthest(rows)
-    south,c = north.furthest(rows)
-  else:
-    tmp = -1
-    for n,one in enumerate(rows):
-      two,dist = one.furthest(rows[n:])
-      it dist > tmp:
-        north, south, c = one, two, tmp
-  return north,south,c
+def cluster(rows0, fast):
+  def find2FarPoints(rows):
+    if fast:
+      anyone   = random.choice(rows)
+      north,_  = anyone.furthest(rows)
+      south, c = north.furthest(rows)
+    else:
+      c=-1
+      for m,one in enumerate(rows):
+        for n,two in enumerate(rows):
+          if n > m and (tmp := one.dist(two)) > c:
+            north, south, c = one, two, tmp
+    return north, south, c
 
-def projects(rows,fast=False,lvl=0):
-  tmp = []
-  north,south,c = poles(rows,fast)
-  for row in rows:
-    a,b = row.dist(north), row.dist(south)
-    x   = (a**2 + c**2 - b**2)/(2*i.c)
-    x   = math.max(0, math,min(1,x))
-    y   = (x**2 - a**2)**.5
-    if lvl==0: row.x,row.y = x,y
-    tmp += [(x, row.x, row.y, row)]
-  mid  = len(tmp) // 2.
-  rows = [z[-1] for z in sorted(tmp)]
-  north,south = rows[:mid], rows[mid:]
-  xs   = sorted(tmp, lambda z:z[1])
-  ys   = sorted(tmp, lambda z:z[2])
-  return it(rows = rows, north=north, south=south, c=c,
-            x0 = xs[0][0], xmid = xs[mid][0], x1 = xs[-1][0],
-            y0 = xs[0][1], ymid = xs[mid][1], y1 = xs[-1][1],
-            up=None,down=None)
-            north,
-            south)
+  def projects(rows, lvl):
+    north,south,c = find2FarPoints(rows)
+    tmp = []
+    for row in rows:
+      a,b = row.dist(north), row.dist(south)
+      x = math.max(0, math,min(1, (a**2 + c**2 - b**2)/(2*c)))
+      y = (x**2 - a**2)**.5
+      if lvl==0: row.x,row.y = x,y
+      tmp += [(x, row.x, row.y, row)]
+    mid         = len(tmp) // 2.
+    rows        = [z[-1] for z in sorted(tmp)]
+    north,south = rows[:mid], rows[mid:]
+    xs,ys       = sorted(tmp, lambda z:z[1]), sorted(tmp, lambda z:z[2])
+    return north, south, it(
+        _rows=rows, north=north, south=south, c=c, up=None, down=None,
+        x0= xs[0][0], xmid= xs[mid][0], x1= xs[-1][0],
+        y0= xs[0][1], ymid= xs[mid][1], y1= xs[-1][1])
 
-def tree(rows, lo, lvl=0)
-  lo = lo or len(rows)**.5
-  if  len(rows) > lo*2:
-    here,down0,up0 = projects(rows,lvl)
-    here.down      = tree(down0, lo, lvl+1)
-    here.up        = tree(up0,   lo, lvl+1)
-    return here
+  def tree(rows, lo,  lvl):
+    if len(rows) > lo*2:
+      down0, up0, here = projects(rows,  lvl)
+      here.down        = tree(down0, lo, lvl+1)
+      here.up          = tree(up0,   lo, lvl+1)
+      return here
+  ###################################
+  return tree(rows0, len(rows)**.5, 0)
 
-def tprint(here, lvl=0):
+def show(here, lvl=0):
   if here:
      print(("|.. "*lvl) + f"{len(here.rows)}")
-     tprint(here.up)
-     tprint(here.down)
+     show(here.up,   lvl+1)
+     show(here.down, lvl+1)
 
 def fastmap(src):
   with open(file) as fp:
     for a in fp:
       yield [atom(x) for x in re.sub(ignore, '', a).split(sep)]
-
-def atom(x):
-  "Coerce x to the right kind of string (int, float, or string)"
-  try: return int(x)
-  except Exception:
-    try:              return float(x)
-    except Exception: return x
 
 __name__ == "__main__" and anExample()
