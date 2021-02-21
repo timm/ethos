ff3224df3a61672f678148a244a93949cfc5244d
Tim Menzies <timm@ieee.org>
2021-02-20 21:21:11 -0500

    saving

--- src/fastmapeg.sh
+++ src/fastmapeg.sh
@@ -4,6 +4,25 @@ python3 fastmap.py > /tmp/$$
 
 cat <<EOF|gnuplot
 unset key
+#set palette defined ( 0 "red", 0.5 "white", 1 "blue" )
+
+
+
+set palette defined ( .1 "#7f0000",\
+                      .2 "#ee0000",\
+                      .3 "#ff7000",\
+                      .4 "#ffee00",\
+                      .5 "#90ff70",\
+                      .6 "#0fffee",\
+                      .7 "#0090ff",\
+                      .8 "#000fff",\
+                      .9 "#000090")
+
+
+
+
+
+
 set terminal pngcairo
 set output '/tmp/fmeg.png'
 plot '/tmp/$$' using 1:2:3 with points pt 11 ps 2 palette
