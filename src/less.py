dd01a3c73fc4a5d8aaaa86201b62c3a6dc6d2bb7
Tim Menzies <timm@ieee.org>
2021-02-21 21:22:40 -0500

    saving

--- src/less.py
+++ src/less.py
@@ -1,176 +1,92 @@
+#!/usr/bin/env python3
+# vim: ts=2 sw=2 sts=2 et tw=81 fdm=indent:
+import types
+from random import random as r
+from random import choice
+import random,sys,re
 
-import randon,sys,re
-from types import FunctionType as fun
-
-r=random.random
-no="?"
-maxnum=128
-
-def same(x): return x
-def lst2xy(lst, prep,ys,xs):
-   i,x,y=[],[], it(x=[], y={})
-   for n,x in enumerate(lst):
-     x = preps[n](x)
-     if  n in ys: i.y += [x)]
-     if  n in xs: i.x += [x]
-   return i
-
-def add(i,x,f): 
-  if x !=no: 
-    i.n += 1; f(i,x); 
-  return x
-
-def Count(pos=0,txt=""): return o(pos=pos,txt=txt,n=0,_all={},max=0,mode=None)
-def Some( pos=0,txt=""): return o(pos=pos,txt=txt,n=0,_all=[],ok=True)
-
-def count(i,x):
-  tmp, i._all[x] = i._all.get(x,0) + 1
-  if tmp>i.max: i.max,i.mode=tmp,x
- 
-def some(i,x):
-  a=i._all
-  if    len(a) < maxnum): a += [x]
-  elif: r() < i.n/maxnum: a[ int(r()*len(a)) ] = x
-  i.ok=False
-
-def arranged(i) :
-  i._all = i._all if i.ok else sorted(i._all)
-  i.ok=True
-  return i._all
-
-def norm(i,x): a=all(i); return (x-a[0])/(a[-1] - a[0] + 1E-32) 
-
-lambda i,x: add(i,float(x), count) 
-
-def 
-   ys = [float(lst[n]) for in goals]
-   xs = [(float(x) if n in xnums else x) for n,x in enumuerate(indep)]
-
-   infor n,z in enumerate(lst):
-     if n in goals: 
-     z=float(z)
-        y += [float(z
+def fun(x): return type(x) == types.FunctionType
 
 class o:
-  "Simple container of names fields, with methods."
-  def __init__(i, **d) : i.__dict__.update(d)
-  def __repr__(i)      : return "{"+ ', '.join( 
-      [f":{k} {v}" for k, v in sorted(i.__dict__.items())
-       if  type(v) != fun and k[0] != "_"])+"}"
-
-
-def main:(src):
-  for x in src:
-    keep([atom(y) for y in 
-          re.sub(r'([\n\t\r ]|#.*)','',x).split(',')])
-
-
-  for line in sys.stdin: NO
-  """Helper function: read csv rows, skip blank lines, 
-  coerce strings
-     to numbers, if needgnore, '', a).split(sep)]
-
-def atom(x):
-  "Coerce x to the right kind of string (int, float, or string)"
-  try: return int(x)
-  except Exception:
-    try:              return float(x)
-    except Exception: return x
-
-__name__ == "__main__" and main(sys.stdin)
-
-
-
-#i!/usr/bin/env python3
-# vim: ts=2 sw=2 sts=2 et tw=81:
-"""
-Reads rows from csv file.
-
-- License: (c) 2021 Tim Menzies <timm@ieee.org>, MIT License  
-
-"""
-import re
-def anExample():
-  rows=[row for row in csv("../data/auto93.csv")]
-  assert 399== len(rows)
-  assert float is type(rows[1][4])
-  assert int   is type(rows[1][0])
-
-def csv(file, sep=",", ignore=r'([\n\t\r ]|#.*)'):
-  """Helper function: read csv rows, skip blank lines, coerce strings
-     to numbers, if needed."""
-  with open(file) as fp:
-    for a in fp:
-      yield [atom(x) for x in re.sub(ignore, '', a).split(sep)]
-
-def atom(x):
-  "Coerce x to the right kind of string (int, float, or string)"
-  try: return int(x)
-  except Exception:
-    try:              return float(x)
-    except Exception: return x
-
-__name__ == "__main__" and anExample()
-
-
-
-#i!/usr/bin/env python3
-# vim: ts=2 sw=2 sts=2 et tw=81:
-"""
-Very succinct object system that bundles nested functions and some get set
-methods into a container.
-
-- License: (c) 2021 Tim Menzies <timm@ieee.org>, MIT License  
-- Example: see the [demoIt](#it.demoIt) function.
-
-"""
-from types import FunctionType as Fun
-
-class it:
-  "Simple container of names fields, with methods."
-  def __init__(i, **d)     : i.__dict__.update(d)
-  def __getitem__(i, k)    : return i.__dict__[k]
-  def __setitem__(i, k, v) : i.__dict__[k] = v
-  def __repr__(i):
-    "Pretty print, sorted keys, ignore private keys (those  with `_`)."
-    return "{"+ ', '.join( 
-      [f":{k} {v}" for k, v in sorted(i.__dict__.items())
-       if  type(v) != Fun and k[0] != "_"])+"}"
-
-  def __add__(i, maybe):
-    "For all functions, add them as methods to `i`."
-    def method(i,f): 
-      return lambda *lst, **kw: f(i, *lst, **kw)
-    def memo(i,k,f):
-      def worker(*l, **kw):
-        if k not in i._memos: i._memos[k] = f(*l, *kw)
-        return i._memos[k]
-      return worker
-    for k,f in maybe.items():
-      if type(f) == Fun and k[0] != "_": 
-        if k[-1] == "0":
-          i._memos = {}
-          k = k[:-1]
-          f = memo(i,k,f)
-        i[k] = method(i,f) 
+  def __init__(i, **d)   : i.__dict__.update(d)
+  def __getitem__(i,k)   : return i.__dict__[k]
+  def __setitem__(i,k,v) : i.__dict__[k] = v
+  def __or__(i,j): 
+    for k in j.__dict__: i[k] = j[k]
+    return i
+  def __repr__(i): return "{"+ ', '.join([f":{k} {v}" 
+    for k, v in sorted(i.__dict__.items()) if not fun(v) and k[0] != "_"])+"}"
+  def __add__(i,d):
+    def method(i,f): return lambda *lst, **kw: f(i, *lst, **kw)
+    for k,v in d.items():
+      if fun(v): i.__dict__[k] = method(i,v)
     return i
 
-def anExample():
-  from datetime import datetime as date
-  def Person(name="Abraham",yob=1809):
-    def age(i)   : return date.now().year - i.yob
-    def heavy0(i): print(1); return i.age() + i.age()*10
-    def exp0(i)  : print(2); return i.age() + i.age()*100
-    return it(name=name, smarts=0,yob=yob,weight=0) + locals()
-  #---------------------------
-  p = Person(name="John")
-  print(p)
-  p.heavy();  
-  p.heavy();  
-  p.heavy();  
-  p.exp();  
-  p.exp();  
-  p.exp();  
-  print(p._memos)
+the=o(no="?", maxnum=128)
 
-__name__ == "__main__" and anExample()
+def same(x): return x
+
+def column( pos=0,txt=""): 
+  what = (Col if the.no in txt else (Num if txt[0].isupper() else Sym))
+  return what(pos=pos, txt=txt) 
+
+def Col(pos=0, txt=""): 
+  def add(i,x) : 
+    if x != the.no: i.n+=1
+    return x
+  return o(pos=pos, txt=txt, n=0, w= (-1 if "-" in txt else 1))  + locals()
+
+def Num(pos=0, txt=""): 
+  def add(i,x) : 
+    if x != the.no:
+      i.n += 1
+      x = float(x)
+      if len(i._all) < i.max: 
+        i.ok=False; i._all += [x]
+      elif r() < i.n / i.max: 
+        i.ok=False; i._all[ int(r()*len(i._all)) ] = x
+    return x
+  def all(i):
+    i._all = i._all if i.ok else sorted(i._all)
+    i.ok = True
+    return i._all
+  def mid(i): a=i.all(); return a[int(len(a)/2)]
+  return (Col(pos,txt) | o(_all=[], max=64, ok=False)) + locals()
+
+def Sym(pos=0, txt=""): 
+  def add(i,x):
+    if x != the.no:
+      i.n += 1
+      tmp, i._all[x] = i._all.get(x,0) + 1
+      if tmp>i.max: i.max,i.mode = tmp,x
+    return x
+  def mid(i): return i.mode
+  return (Col(pos,txt) | o(_all={}, mode=None, max=0)) + locals()
+
+def sd(i,x): 
+  a=i.all(); return (a[int(.9*len(a))] - a[int(.1*len(a))])/2.56
+
+def About(lst):
+  i = o(header=lst, x=[], y=[], all=[])
+  for pos,txt in enumerate(lst):
+    one = column(pos,txt)
+    i.all.append(one)
+    if the.no not in txt: 
+      (i.y if txt[0] in "+-" else i.x).append(one)
+  return i
+
+def data(src, about=None):
+  about = about or About(next(src))
+  for row in rows(src):
+    yield about, [col.add(col,x) for col,x in zip(about.all,row)]
+
+def rows(src):
+  for line in src:
+    yield re.sub(r'([\n\t\r ]|#.*)','',line).split(",")
+
+def csv(file):
+   with open(file) as fp:
+     for lst in fp: yield lst
+
+for _,row in data(rows(csv("../data/auto93.csv"))): 
+  print(row)
